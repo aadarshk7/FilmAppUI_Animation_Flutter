3f6e9afed4a845a48d675619bccad012c679f275
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-10 09:30:58 +0545

    Designing UI

---
+++ lib/pageview_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PageViewScreen extends StatefulWidget {
+  const PageViewScreen({super.key});
+
+  @override
+  State<PageViewScreen> createState() => _PageViewScreenState();
+}
+
+class _PageViewScreenState extends State<PageViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello"),
+    );
+  }
+}
